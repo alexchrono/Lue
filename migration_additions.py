9547ec44d48856d58f6a0f231f2611e574912a05
Alex Heasley <alex.heasley@gmail.com>
2023-11-01 12:15:06 -0500

    updated migrations

---
+++ migration_additions.py
@@ -0,0 +1,22 @@
+
+# #after imports
+# import os
+# environment = os.getenv("FLASK_ENV")
+# SCHEMA = os.environ.get("SCHEMA")
+
+
+
+# #between upgrade and downgrade
+# # add to every migration:
+#     if environment == "production":
+#         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+    # if environment == "production":
+    #     op.execute(f"ALTER TABLE dailies SET SCHEMA {SCHEMA};")
+    # if environment == "production":
+    #     op.execute(f"ALTER TABLE habits SET SCHEMA {SCHEMA};")
+    # if environment == "production":
+    #     op.execute(f"ALTER TABLE songs SET SCHEMA {SCHEMA};")
+    # if environment == "production":
+    #     op.execute(f"ALTER TABLE likes SET SCHEMA {SCHEMA};")
+    # if environment == "production":
+    #     op.execute(f"ALTER TABLE playlist_songs SET SCHEMA {SCHEMA};")
