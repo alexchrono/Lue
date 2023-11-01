c55c3294e52adaf429e08734eb610c3e92945686
Alex Heasley <alex.heasley@gmail.com>
2023-11-01 16:10:39 -0500

    before i mess with user signup form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
