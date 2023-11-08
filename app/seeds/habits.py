255d6568f1aac94775d74bcca9f84bbce7bc74a2
Alex Heasley <alex.heasley@gmail.com>
2023-11-08 13:46:25 -0600

    made seeds a lot better.  now lets mess with order.

--- app/seeds/habits.py
+++ app/seeds/habits.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import text
 habits_list = [
     Habit(
         title='Drink Water',
-        notes='Drink atleast 8 glasses of water a day',
+        notes='Drink at least 8 glasses of water a day',
         difficulty=1,
         position=1,
         reset_rate='daily',
@@ -27,16 +27,160 @@ habits_list = [
         difficulty=2,
         position=3,
         reset_rate='daily',
-        alignment=False, 
+        alignment=False,
         user_id=1
     ),
     Habit(
         title='Late Night Snacking',
-        notes='Do not eat after 9 PM.  Im getting fat',
+        notes='Do not eat after 9 PM. I\'m getting fat',
         difficulty=3,
         position=4,
         reset_rate='daily',
         alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='Daily Stretching',
+        notes='Do a full-body stretching routine in the morning',
+        difficulty=1,
+        position=5,
+        reset_rate='daily',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Use Stairs',
+        notes='Use the stairs instead of the elevator for a month',
+        difficulty=2,
+        position=6,
+        reset_rate='monthly',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Fast Food',
+        notes='Limit fast food to only once a week',
+        difficulty=3,
+        position=7,
+        reset_rate='weekly',
+        alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='Flossing',
+        notes='Floss every night before bed',
+        difficulty=1,
+        position=8,
+        reset_rate='daily',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Junk Food',
+        notes='No more candy or chips',
+        difficulty=2,
+        position=9,
+        reset_rate='daily',
+        alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='Gratitude Journal',
+        notes='Write three things you\'re grateful for',
+        difficulty=1,
+        position=10,
+        reset_rate='daily',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Procrastination',
+        notes='Do not procrastinate on tasks',
+        difficulty=3,
+        position=11,
+        reset_rate='daily',
+        alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='pre-cooking Meals',
+        notes='pre-cook healthy meals at home every sunday',
+        difficulty=2,
+        position=12,
+        reset_rate='weekly',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Snacking',
+        notes='Eliminate snacking between meals',
+        difficulty=2,
+        position=13,
+        reset_rate='daily',
+        alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='Yoga',
+        notes='Practice yoga every morning',
+        difficulty=1,
+        position=14,
+        reset_rate='daily',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Online Shopping',
+        notes='Reduce unnecessary online purchases',
+        difficulty=3,
+        position=15,
+        reset_rate='weekly',
+        alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='Reading before bed',
+        notes='Read for 30 minutes before sleep',
+        difficulty=1,
+        position=16,
+        reset_rate='daily',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Soda Drinks',
+        notes='Cut out soda from your diet',
+        difficulty=2,
+        position=17,
+        reset_rate='daily',
+        alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='Morning Meditation',
+        notes='Meditate each morning for 10 minutes',
+        difficulty=1,
+        position=18,
+        reset_rate='daily',
+        alignment=True,
+        user_id=1
+    ),
+    Habit(
+        title='Negativity',
+        notes='Avoid negative self-talk throughout the day',
+        difficulty=3,
+        position=19,
+        reset_rate='daily',
+        alignment=False,
+        user_id=1
+    ),
+    Habit(
+        title='Improve social life',
+        notes='Try to go on a date every week- I need to find misses right!',
+        difficulty=2,
+        position=20,
+        reset_rate='weekly',
+        alignment=True,
         user_id=1
     ),
     Habit(
--- migrations/versions/7ab821fa1766_.py
+++ migrations/versions/7ab821fa1766_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 01f4d834e8c9
+Revision ID: 7ab821fa1766
 Revises: 
-Create Date: 2023-11-08 12:55:25.804350
+Create Date: 2023-11-08 13:43:19.820809
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '01f4d834e8c9'
+revision = '7ab821fa1766'
 down_revision = None
 branch_labels = None
 depends_on = None
