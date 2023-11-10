d54c10bae700978a790a04a6326d4e27dd721951
Alex Heasley <alex.heasley@gmail.com>
2023-11-09 22:27:32 -0600

    added duckhunt dog and improved error modal

--- react-app/src/components/errorShow/index.js
+++ react-app/src/components/errorShow/index.js
@@ -8,6 +8,10 @@ export default function ErrorComponent({ errorMessage,setErrors,setHabit }) {
     return (
         <>
       <div className="error-popup">
+      <div class='victory-celebrate'>
+                    <img
+                  src={`${process.env.PUBLIC_URL}/icons/duckhuntDog.gif`}></img>
+                  </div>
         <h1>Error</h1>
        <p class='para'>{errorMessage}</p>
         <button onClick={(e)=>{
