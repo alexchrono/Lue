de227c1994be0ed8c21a485847efd151b08560bb
Alex Heasley <alex.heasley@gmail.com>
2023-11-12 18:35:15 -0600

    made login form a lot better

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -22,35 +22,44 @@ function LoginFormModal() {
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
-    </>
+
+    <div className='overallSizeEditModal2'>
+  <h1 className='login'>Log In</h1>
+  <div className='flexLogin'>
+    <form onSubmit={handleSubmit}>
+      <ul>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+      <p className='Label'>Email</p>
+
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+      <p className='Label'>Password</p>
+
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+      <div className='loginButtonContainer'>
+        <button className='letsMakePretty' onClick={(e)=>{
+          e.stopPropagation();
+          closeModal()
+        }}>Cancel</button><button className='letsMakePretty' type="submit">Log In</button>
+      </div>
+    </form>
+  </div>
+</div>
+
   );
 }
 
